62887549a9e60a43bd52706a15e208b66f229ddc
Inka-Liina Lauranto <inka-liina.lauranto@edu.lapinamk.fi>
2025-03-05 15:31:39 +0200

    Lisätty fitted_text_clf.pkl-tiedosto. Lisätty nlp.py-tiedosto, jossa fitted_text_clf-muuttujaan haetaan fitted_text_clf.pkl-tiedostosta fitted_text_clf-muuttujaan koulutettu analyysimalli. app.py-tiedostossa /sentiment-routen funktio tehty valmiiksi nlp.py-tiedostossa olevaa koulutusmallimuuttujaa hyödyntämällä.

---
+++ nlp.py
@@ -0,0 +1,10 @@
+import pickle
+
+fitted_text_clf = None
+
+# Avataan serialisoidun koulutetun analyysimallin sisällään pitämä tiedosto:
+with open("./data/fitted_text_clf.pkl", "rb") as f:
+    # Deserialisoidaan tiedoston sisältö ja tallennetaan koulutusmalli 
+    # aiempana alustettuun muuttujaan:
+    fitted_text_clf = pickle.load(f)
+    f.close()
