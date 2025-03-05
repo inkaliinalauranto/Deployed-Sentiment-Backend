03e534125c342ee25d8ad8f35f2db34878e8d9d8
Inka-Liina Lauranto <inka-liina.lauranto@edu.lapinamk.fi>
2025-03-05 16:57:41 +0200

    Kommentoitu edelliseen buildiin lisätyt koodit pois, koska Rahti-build epäonnistui.

--- nlp.py
+++ nlp.py
@@ -1,10 +1,10 @@
-import pickle
+# import pickle
 
-fitted_text_clf = None
+# fitted_text_clf = None
 
-# Avataan serialisoidun koulutetun analyysimallin sisällään pitämä tiedosto:
-with open("./data/fitted_text_clf.pkl", "rb") as f:
-    # Deserialisoidaan tiedoston sisältö ja tallennetaan koulutusmalli 
-    # aiempana alustettuun muuttujaan:
-    fitted_text_clf = pickle.load(f)
-    f.close()
+# # Avataan serialisoidun koulutetun analyysimallin sisällään pitämä tiedosto:
+# with open("./data/fitted_text_clf.pkl", "rb") as f:
+#     # Deserialisoidaan tiedoston sisältö ja tallennetaan koulutusmalli 
+#     # aiempana alustettuun muuttujaan:
+#     fitted_text_clf = pickle.load(f)
+#     f.close()
