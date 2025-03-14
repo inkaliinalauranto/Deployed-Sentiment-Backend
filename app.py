ebd13c1ff06e3ba967ed5d8cae3ce2f506c97044
Inka-Liina Lauranto <inka-liina.lauranto@edu.lapinamk.fi>
2025-03-14 20:42:56 +0200

    Lisätty uudellen CORS-paketti, joka otettu käyttöön sallivilla luvilla.

--- app.py
+++ app.py
@@ -1,14 +1,16 @@
 from flask import Flask
+from flask_cors import CORS
 from controllers.sentiments import get_sentiment, hello_world
 
 app = Flask(__name__)
+
+CORS(app, origins=["*"])
 
 app.add_url_rule(rule="/", view_func=hello_world)
 
 app.add_url_rule(rule="/api/sentiment",
                  view_func=get_sentiment,
                  methods=["POST"])
-
 
 # if __name__ == '__main__':
 #     app.run(host="0.0.0.0", port="8080", debug=False)
