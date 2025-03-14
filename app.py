08f020d9acebff451d331ede23dcd5cce34fceb5
Inka-Liina Lauranto <inka-liina.lauranto@edu.lapinamk.fi>
2025-03-14 17:38:09 +0200

    Muutoksia endpointeihin ja CORS-policyyn, jotta endpointit saadaan toimimaan myös Rahdin kautta.

--- app.py
+++ app.py
@@ -6,11 +6,11 @@ app = Flask(__name__)
 
 # Sallitaan frontendin locahostin origin ja Renderissä käyttöönotettuun 
 # versioon liittyvä origin:
-CORS(app)
+CORS(app, origins=["*"])
 
-app.add_url_rule(rule="/sentiment", view_func=hello_world)
+app.add_url_rule(rule="/", view_func=hello_world)
 
-app.add_url_rule(rule="/",
+app.add_url_rule(rule="/api/sentiment",
                  view_func=get_sentiment,
                  methods=["POST"])
 
