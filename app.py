59068a649b6792d8b599a7e1b84e5aa646d7d561
Inka-Liina Lauranto <inka-liina.lauranto@edu.lapinamk.fi>
2025-03-14 18:14:45 +0200

    Peruutettu refaktorointia Rahti-ongelman selvittämiseksi.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_cors import CORS
 from controllers.sentiments import get_fake_sentiment, get_sentiment, hello_world
 
@@ -8,13 +8,22 @@ app = Flask(__name__)
 # versioon liittyvä origin:
 # CORS(app, origins=["*"])
 
-app.add_url_rule(rule="/", view_func=hello_world)
+# app.add_url_rule(rule="/", view_func=hello_world)
 
-app.add_url_rule(rule="/api/fake-sentiment", view_func=get_fake_sentiment)
+# app.add_url_rule(rule="/api/fake-sentiment", view_func=get_fake_sentiment)
 
-app.add_url_rule(rule="/api/sentiment",
-                 view_func=get_sentiment,
-                 methods=["POST"])
+# app.add_url_rule(rule="/api/sentiment",
+#                  view_func=get_sentiment,
+#                  methods=["POST"])
+
+
+@app.route("/")
+def hello_world():
+    return jsonify({"Message": "Hello world!"})
+
+@app.route("/api/fake-sentiment")
+def get_fake_sentiment():
+    return jsonify({"result": "positive"})
 
 # if __name__ == '__main__':
 #     app.run(host="0.0.0.0", port="8080", debug=False)
