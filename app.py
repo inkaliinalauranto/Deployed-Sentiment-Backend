848f5a91bfbe4b5290a2b955d8d150d401c67af4
Inka-Liina Lauranto <inka-liina.lauranto@edu.lapinamk.fi>
2025-03-14 20:20:40 +0200

    Revert "Lisätty CORS-paketti ja sen käyttöönotto."
    This reverts commit ecb728f2f80752a5b17bad4cdaefe460f99660c3.

--- app.py
+++ app.py
@@ -1,29 +1,14 @@
-from flask import Flask, jsonify
-from flask_cors import CORS
-from controllers.sentiments import get_fake_sentiment, get_sentiment, hello_world
+from flask import Flask
+from controllers.sentiments import get_sentiment, hello_world
 
 app = Flask(__name__)
 
-# Sallitaan frontendin locahostin origin ja Renderissä käyttöönotettuun 
-# versioon liittyvä origin:
-# CORS(app, origins=["*"])
+app.add_url_rule(rule="/", view_func=hello_world)
 
-# app.add_url_rule(rule="/", view_func=hello_world)
+app.add_url_rule(rule="/api/sentiment",
+                 view_func=get_sentiment,
+                 methods=["POST"])
 
-# app.add_url_rule(rule="/api/fake-sentiment", view_func=get_fake_sentiment)
-
-# app.add_url_rule(rule="/api/sentiment",
-#                  view_func=get_sentiment,
-#                  methods=["POST"])
-
-
-@app.route("/")
-def hello_world():
-    return jsonify({"Message": "Hello world!"})
-
-@app.route("/api/fake-sentiment")
-def get_fake_sentiment():
-    return jsonify({"result": "positive"})
 
 # if __name__ == '__main__':
 #     app.run(host="0.0.0.0", port="8080", debug=False)
