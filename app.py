ecb728f2f80752a5b17bad4cdaefe460f99660c3
Inka-Liina Lauranto <inka-liina.lauranto@edu.lapinamk.fi>
2025-03-14 16:27:08 +0200

    Lisätty CORS-paketti ja sen käyttöönotto.

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
 from flask import Flask
+from flask_cors import CORS
 from controllers.sentiments import get_sentiment, hello_world
 
 app = Flask(__name__)
+
+# Sallitaan frontendin locahostin origin ja Renderissä käyttöönotettuun 
+# versioon liittyvä origin:
+CORS(app, origins=["http://localhost:5173", "https://deployed-sentiment-analysis-frontend.onrender.com/"])
 
 app.add_url_rule(rule="/", view_func=hello_world)
 
 app.add_url_rule(rule="/api/sentiment",
                  view_func=get_sentiment,
                  methods=["POST"])
-
 
 # if __name__ == '__main__':
 #     app.run(host="0.0.0.0", port="8080", debug=False)
