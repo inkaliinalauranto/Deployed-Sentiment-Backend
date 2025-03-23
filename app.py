9a64b1c587502ba3403c4f8c74eb27ee08c40852
Inka-Liina Lauranto <inka-liina.lauranto@edu.lapinamk.fi>
2025-03-23 10:54:59 +0200

    CORS-käytänteihin lisätty lisämäärittelyjä, jotta API:n käyttäminen toimii käyttönotetuissa frontendeissa.

--- app.py
+++ app.py
@@ -4,15 +4,19 @@ from controllers.sentiments import get_sentiment, hello_world
 
 app = Flask(__name__)
 
-CORS(app, origins=["http://localhost:5500", 
-                   "https://kind-forest-04e83171e.6.azurestaticapps.net/", 
-                   "https://deployed-sentiment-analysis-frontend.onrender.com/"])
+CORS(
+    app,
+    origins=[
+        "http://localhost:5500",
+        "https://kind-forest-04e83171e.6.azurestaticapps.net/",
+        "https://deployed-sentiment-analysis-frontend.onrender.com/",
+    ],
+    supports_credentials=True,
+)
 
 app.add_url_rule(rule="/", view_func=hello_world)
 
-app.add_url_rule(rule="/api/sentiment",
-                 view_func=get_sentiment,
-                 methods=["POST"])
+app.add_url_rule(rule="/api/sentiment", view_func=get_sentiment, methods=["POST"])
 
 # if __name__ == '__main__':
 #     app.run(host="0.0.0.0", port="8080", debug=False)
