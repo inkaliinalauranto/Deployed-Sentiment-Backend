50b8d62a4ff17784b6b9c43dab191efe0e86aeae
Inka-Liina Lauranto <inka-liina.lauranto@edu.lapinamk.fi>
2025-03-28 19:13:59 +0200

    app.py-tiedostossa autentikaatioon liittyvät koodirivit otettu takaisin käyttöön.

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from init_db import engine
 load_dotenv()
 
 # Luodaan tietokantataulut, jos niitä ei vielä ole:
-# Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
 
 # Alustetaan Flask-luokan instanssi:
 app = Flask(__name__)
@@ -28,11 +28,11 @@ CORS(
 app.add_url_rule(rule="/", view_func=hello_world)
 app.add_url_rule(rule="/api/sentiment", view_func=get_sentiment, methods=["POST"])
 
-# app.add_url_rule(rule="/api/users", view_func=get_users)
-#
-# app.add_url_rule(rule="/api/register", view_func=register, methods=["POST"])
-# app.add_url_rule(rule="/api/login", view_func=login, methods=["POST"])
-# app.add_url_rule(rule="/api/account", view_func=get_account)
+app.add_url_rule(rule="/api/users", view_func=get_users)
+
+app.add_url_rule(rule="/api/register", view_func=register, methods=["POST"])
+app.add_url_rule(rule="/api/login", view_func=login, methods=["POST"])
+app.add_url_rule(rule="/api/account", view_func=get_account)
 
 # if __name__ == '__main__':
 #     app.run(host="0.0.0.0", port="8080", debug=False)
